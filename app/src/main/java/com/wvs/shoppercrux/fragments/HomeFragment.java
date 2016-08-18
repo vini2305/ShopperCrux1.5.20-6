6335aa6bb40ce47f04c17c71162de9819eeb1d26
win7 <vinayaka@wvs.in>
2016-08-18 18:51:48 +0530

    updated 18-8

--- app/src/main/java/com/wvs/shoppercrux/fragments/HomeFragment.java
+++ app/src/main/java/com/wvs/shoppercrux/fragments/HomeFragment.java
@@ -35,6 +35,8 @@ import com.wvs.shoppercrux.activities.MainActivity;
 import com.wvs.shoppercrux.helper.SQLiteHandler;
 import com.wvs.shoppercrux.helper.SessionManager;
 
+
+// we will do it in 19-8-16
 public class HomeFragment extends Fragment implements GoogleMap.OnMyLocationButtonClickListener, View.OnClickListener,
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback,LocationListener {
